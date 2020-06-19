57f9b1ae437420714e1287ade96e295b0f4c880d
C1296135 Eduardo Soares Sampaio <c1296135@interno.bb.com.br>
2020-06-19 10:59:12 -0300

    atualizando site

--- scripts/resume-awards.js
+++ scripts/resume-awards.js
@@ -2,25 +2,21 @@
 Vue.component('resume-awards', {
     props: ['resume'],
     template:
-    `<section class="resume-section reference-section mb-5">
+`
+    <section class="resume-section reference-section mb-5">
     <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Awards</h2>
     <div class="resume-section-content">
         <ul class="list-unstyled resume-awards-list">
-            <li class="mb-2 pl-4 position-relative">
+            <li v-for="award  in resume.awards" class="mb-2 pl-4 position-relative">
                 <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
-                <div class="resume-award-name">Award Name Lorem</div>
-                <div class="resume-award-desc">Award desc goes here, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.</div>
-            </li>
-            <li class="mb-0 pl-4 position-relative">
-                <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
-                <div class="resume-award-name">Award Name Ipsum</div>
-                <div class="resume-award-desc">Award desc goes here, ultricies nec, pellentesque.</div>
+                <div class="resume-award-name">{{award.name}}</div>
+                <div v-if="award.description" class="resume-award-desc">{{award.description}}</div>
             </li>
         </ul>
     </div>
 </section>
+`
 
-    `
 });
 
 
